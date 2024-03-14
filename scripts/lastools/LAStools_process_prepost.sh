13631abe1fe40ece773df875efa38e07c772d1d7
Alex Cebulski <alexcebulski@gmail.com>
2024-03-14 13:56:37 -0600

    abstracted out the config defs to sep file

--- scripts/lastools/LAStools_process_prepost.sh
+++ scripts/lastools/LAStools_process_prepost.sh
@@ -9,8 +9,24 @@
 
 cur_datetime=$(date +"%Y-%m-%d-%H-%M-%S")
 
+# Check if the config file argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <config_file>"
+    exit 1
+fi
+
+# Get the config file path from the command line argument
+config_file="$1"
+
+# Check if the config file exists
+if [ ! -f "$config_file" ]; then
+    echo "Error: File $config_file not found."
+    exit 1
+fi
+
+source $config_file
+
 #### las params ####
-n_cores=6 # this works for functions that run on tiles but only if we run the functions from the absolute path for some reason
 z_min=2050 # drop pts below this ele
 z_max=2100 # drop pts above this ele
 # rm_noise_step=3 # use a [n]x[n]x[n] uniform grid for finding isolated points  
@@ -27,29 +43,6 @@ las2dem_float_prec=0.00025 # as in staines 2023
 las2dem_max_tin_edge=0.1 # as in staines 2023
 tile_size=50
 buffer=5
-
-#### proj settings ####
-las_path=/home/alex/bin/LAStools/bin
-prj_dir=/media/alex/phd-data/local-usask/analysis/lidar-processing
-shp_clip=${prj_dir}/data/gis/shp/fsr_traj_extent_buff_20m.shp
-# file_list="23_072_FT_new 23_073_FT_new"
-file_list="23_026_FT_new 23_027_FT_new"
-pre_sf_index=1
-post_sf_index=2
-# Convert the file list string into an array
-read -r -a file_array <<< "$file_list"
-# Extract elements based on indices
-pre_sf="${file_array[$pre_sf_index - 1]}"
-post_sf="${file_array[$post_sf_index - 1]}"
-
-pt_cld_path="/media/alex/phd-data/local-usask/field-downloads/lidar-data/pointclouds"
-
-prj_name=params_v1.0.0 # for file name suffix
-# prj_name='base_pars' # for prj dirs and file name suffix
-
-out_path=${prj_dir}/data/processed
-log_file=${prj_dir}/logs/lastools/${cur_datetime}_${prj_name}_lidar_pre_post_processing.log
-
 
 echo "######################################################################" | tee -a $log_file
 echo LAStools_process_prepost.sh script started at $cur_datetime under the project name $prj_name | tee -a $log_file
