14ce4720239b0e9521e67ba61f48540fca7ae115
Alex Cebulski (zvd094) <zvd094@cniclgn002.cm.cluster>
2024-05-20 17:05:22 -0600

    remote changes to las merge script

--- scripts/lastools/copernicus_las_merge_clip.sh
+++ scripts/lastools/copernicus_las_merge_clip.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=6:00:00
 #SBATCH --job-name=fortress-lidar
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8G
 # for regular GIWS compute note --account=hpc_c_giws_clark
 # for the large memory compute note --account=hpc_c_giws_mem_clark (100 TB and 80 cores)
@@ -12,22 +12,28 @@
 #SBATCH --error=/globalhome/zvd094/HPC/lidar-processing/scripts/lastools/slurm-logs/slurm_%A_%a.err
 #SBATCH --out=/globalhome/zvd094/HPC/lidar-processing/scripts/lastools/slurm-logs/slurm_%A_%a.out
 
-module load gentoo/2020
+#module load gentoo/2020
 module load singularity/3.9.2
 
+export LAStoolsLicenseFile=/globalhome/zvd094/HPC/license/lastoolslicense.txt # add license to path so we can use funcitons
 las_path="singularity exec -B /gpfs /opt/software/singularity-images/LAStools.sif /opt/lastools/bin"
 
 shp_clip_new="/gpfs/tp/gwf/gwf_cmt/zvd094/fortress/lidar-processing/data/gis/shp/fsr_traj_extent_buff_20m.shp"
 
-# pt_cld_path="/media/alex/phd-data/local-usask/analysis/lidar-processing/data/processed/23_072"
-pt_cld_path="/globalhome/zvd094/HPC/sym_link_gwf_prj/fortress/lidar-processing/data/pointclouds_stripalign/23_072"
-pt_cld_path_out="/globalhome/zvd094/HPC/sym_link_gwf_prj/fortress/lidar-processing/data/pointclouds_stripalign_clipmerge/23_072"
+event_list=("23_026" "23_027" "23_072" "23_073")
 
-shp_clip_new="/home/alex/local-usask/analysis/lidar-processing/data/gis/shp/fsr_road_clip.shp"
+for A in "${event_list[@]}"; do
 
-$las_path/lasmerge64 -i $pt_cld_path/*.las \
-        -o "${pt_cld_path_out}/23_072_sa.las"
+    pt_cld_path="/gpfs/tp/gwf/gwf_cmt/zvd094/fortress/lidar-processing/data/pointclouds_stripalign/${A}"
+    pt_cld_path_out="/gpfs/tp/gwf/gwf_cmt/zvd094/fortress/lidar-processing/data/pointclouds_stripalign_clipmerge"
 
-$las_path/lasclip64 "${pt_cld_path_out}/23_072_sa_mrg.las" \
+    # Merge LAS files
+    $las_path/lasmerge64 -v -i $pt_cld_path/*.las \
+        -o $pt_cld_path_out/${A}_sa_mrg_clp.las
+
+    # Clip the merged LAS file
+    $las_path/lasclip64 -v -i $pt_cld_path_out/${A}_sa_mrg_clp.las \
         -poly $shp_clip_new \
-        -o "${pt_cld_path_out}/23_072_sa_mrg_clp.las" -v
+        -o $pt_cld_path_out/${A}_sa_mrg_clp.las
+
+done
